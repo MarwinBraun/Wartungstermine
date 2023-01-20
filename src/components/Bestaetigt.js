a3796ea629ae1a331073c8ef6ca016b306544d69
Administrator <Administrator@dietenmeier.local>
2023-01-20 15:06:04 +0100

    Spinner für Bestätigung durch Kunde hinzugefügt

--- src/components/Bestaetigt.js
+++ src/components/Bestaetigt.js
@@ -6,16 +6,19 @@ import {useParams } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import bild from  '../App_Einstellungen.PNG';
+import Spinner from 'react-bootstrap/Spinner';
 import '../App.css';
 
 const Bestaetigt = () => {
 
   const [bes, setbes] = useState(false);
+  const [LoadingModal, setLoadingModal] = useState(false);
 
     var { id } = useParams();
 
     
     const onLoad = async () => {
+      setLoadingModal(true);
       try {
               const res = await axios.get('http://mail.dietenmeier-harsch.de:81/done.php', {
               params: { id: id}, 
@@ -28,6 +31,7 @@ const Bestaetigt = () => {
              let response = res.data.best;
              
              setbes(true);
+             setLoadingModal(false);
              
             } catch (err) {
               if (err.response.status === 500) {
@@ -37,6 +41,8 @@ const Bestaetigt = () => {
               }
               
             } 
+
+            
           }
 
  
@@ -54,8 +60,18 @@ const Bestaetigt = () => {
 
 <Row>
     <Col xs={12} className="text-center">
+
+    {LoadingModal === true ? (
+        <Row>
+        <Col  xs={12} className="text-center">
+        <Spinner animation="border" variant="primary" />
+        </Col>
+       
+       </Row>
+       ) : (
  
-    {bes === false ? ( 
+    bes === false ? ( 
+     
      <div>
       <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
     <h4>Bitte klicken Sie auf den Button, um Ihren Termin zu bestätigen.</h4><br></br>
@@ -74,7 +90,9 @@ Sollten Sie noch Fragen haben, zögern Sie bitte nicht, sich über <a href="mail
   
 </p>
 </div>
-    )}
+    )
+    )
+     }
 
 
   
